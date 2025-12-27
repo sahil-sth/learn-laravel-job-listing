a7d33ce5272230ca06a8b897cb6198c3246f5f40
sahil-sth <saheel.sth99@gmail.com>
2025-12-27 13:17:13 -0700

    Add the link to the jobs page

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -20,7 +20,7 @@
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
               <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
-              <x-nav-link href="/about"  :active="request()->is('about')">About</x-nav-link>
+              <x-nav-link href="/jobs"  :active="request()->is('jobs')">Jobs</x-nav-link>
               <x-nav-link href="/contact"  :active="request()->is('contact')">Contact</x-nav-link>
             </div>
           </div>
@@ -58,7 +58,7 @@
       <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
         <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
         <a href="/" aria-current="page" class="block rounded-md bg-gray-950/50 px-3 py-2 text-base font-medium text-white">Home</a>
-        <a href="/about" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
+        <a href="/jobs" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Jobs</a>
         <a href="/contact" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
       </div>
       <div class="border-t border-white/10 pt-4 pb-3">
