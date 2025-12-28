839f10af93575c57010d9403c5298ba893346e76
sahil-sth <saheel.sth99@gmail.com>
2025-12-27 23:25:59 -0700

    Add styling to the jobs listing

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,7 +3,7 @@
     <ul>
     @foreach ($jobs as $job)
         <li>
-            <a href="jobs/{{ $job['id'] }}">{{ $job['title'] }}: pays {{ $job['salary']}}</a>
+            <a href="jobs/{{ $job['id'] }}" class="text-blue-300 underline">{{ $job['title'] }}: pays {{ $job['salary']}}</a>
         </li>
     @endforeach
     </ul>
