3f744962ae61d2c35ef462fb727b3dc568f5f778
sahil-sth <saheel.sth99@gmail.com>
2025-12-28 13:33:38 -0700

    Create a factory for User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,14 +24,14 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
-
     /**
      * Indicate that the model's email address should be unverified.
      */
