71e21c0c288d88ac51fb2dfd98979818ab5e0e54
sahil-sth <saheel.sth99@gmail.com>
2025-12-30 13:58:25 -0700

    Add features to verify request

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::get("/jobs/create", function(){
 });
 
 Route::post("/jobs", function(){
+    request()->validate([
+        "title" => ["required", "min:3"], 
+        "salary" => ["required"]
+    ]);
     Job::create([
         "title" => request("title"), 
         "salary" => request("salary"), 
