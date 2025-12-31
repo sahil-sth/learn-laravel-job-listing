6be92360babaf15c070ee657e84bfb2e92d13817
sahil-sth <saheel.sth99@gmail.com>
2025-12-31 13:44:03 -0700

    Show cursor on hover to the button component

--- resources/views/Components/button.blade.php
+++ resources/views/Components/button.blade.php
@@ -1,5 +1,5 @@
 <a
-  {{ $attributes->merge(['class' => 'rounded-md bg-indigo-500 px-4 py-2 text-sm font-semibold text-white hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-900']) }}
+  {{ $attributes->merge(['class' => 'rounded-md bg-indigo-500 px-4 py-2 text-sm cursor-pointer font-semibold text-white  hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-900']) }}
 >
   {{ $slot }}
 </a>
