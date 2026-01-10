8604d42fea595f8f6dba665d8c7aadd248e7a97c
sahil-sth <saheel.sth99@gmail.com>
2026-01-10 09:58:03 -0700

    Scaffold the register web url

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,52 @@
+<x-layout>
+  <x-slot name="heading">Register User</x-slot>
+
+  <form method="POST" action="/jobs">
+    @csrf
+    <div class="space-y-12">
+      <div class="border-b border-white/10 pb-12">
+        <h2 class="text-base/7 font-semibold text-white">Enter the job information:</h2>
+
+        <p class="mt-1 text-sm/6 text-gray-400">We just need a handful of information from you.</p>
+
+        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <!-- Title -->
+          <x-form-field>
+            <x-form-label for="title">Title</x-form-label>
+
+            <div class="mt-2">
+              <x-form-input placeholder="CEO" name="title" id="title" required/>
+            </div>
+            <x-form-error name="title"/>
+          </x-form-field>
+
+          <!-- Salary -->
+          <x-form-field>
+            <x-form-label for="salary">Salary</x-form-label>
+            <div class="mt-2">
+              <div
+                class="flex items-center rounded-md bg-white/5 pl-3 outline-1 -outline-offset-1 outline-white/10 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-500"
+              >
+                
+              </div>
+              <x-form-input   
+                  id="salary"
+                  type="text"
+                  name="salary"
+                  placeholder="$50,000"
+                  required/>
+            </div>
+            @error('salary')
+              <p class="mt-4 text-xs font-semibold text-red-500">{{ $message }}</p>
+            @enderror
+          </x-form-field>
+        </div>
+      </div>
+    </div>
+    <!-- Actions -->
+    <div class="mt-6 flex items-center justify-end gap-x-6">
+      <button type="button" class="text-sm/6 font-semibold text-white">Cancel</button>
+      <x-form-button>Save</x-form-button>
+    </div>
+  </form>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+
 use Illuminate\Support\Facades\Route;
 // static routes
 Route::view("/", "home");
@@ -17,3 +19,5 @@ Route::view("/contact", "contact");
 // });
 
 Route::resource("jobs", JobController::class); // automatically creates what we have up 
+
+Route::get("/register", [RegisteredUserController::class, "create"]);
