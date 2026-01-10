e191e11aef5f56ca485ef4b8a29a61c3a4956d38
sahil-sth <saheel.sth99@gmail.com>
2026-01-09 21:34:04 -0700

    Use resource instead of manually adding routes for job endpoint

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,14 @@ use Illuminate\Support\Facades\Route;
 Route::view("/", "home");
 Route::view("/contact", "contact");
 // Job routes
-Route::get("/jobs", [JobController::class, "index"]);
-Route::get("/jobs/create", [JobController::class, "create"]);
-Route::post("/jobs", [JobController::class, "store"]);
-Route::get('/jobs/{id}', [JobController::class, "show"]);
-Route::patch('/jobs/{id}', [JobController::class, "update"]);
-Route::delete('/jobs/{id}', [JobController::class, "destroy"]);
-Route::get('/jobs/{id}/edit', [JobController::class, "edit"]);
+// Route::controller(JobController::class)->group(function(){
+//     Route::get("/jobs",  "index");
+//     Route::get("/jobs/create",  "create");
+//     Route::post("/jobs",  "store");
+//     Route::get('/jobs/{id}',  "show");
+//     Route::patch('/jobs/{id}',  "update");
+//     Route::delete('/jobs/{id}',  "destroy");
+//     Route::get('/jobs/{id}/edit',  "edit");
+// });
+
+Route::resource("jobs", JobController::class); // automatically creates what we have up 
