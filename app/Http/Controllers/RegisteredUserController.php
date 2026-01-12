cc37d841cd88ed3bffc3585ec115669fa8f3c53a
sahil-sth <saheel.sth99@gmail.com>
2026-01-11 19:29:36 -0700

    Allow users to be registered

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,14 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class RegisteredUserController extends Controller
 {
     function create(){
         return view("auth.register");
     }
-    function store(Request $request){
-        dd(request()->all());
+    function store(){
+        // validate request
+        $validatedAttributes = request()->validate([
+            "first_name" => ["required"], 
+            "last_name"=> ["required"], 
+            "email"=> ["required", "email"],
+            "password"=> ["required", Password::min(5), "confirmed"] 
+
+        ]);
+        // create a user in the database
+        $user = User::create($validatedAttributes);
+        // authenticate the user or sign them in 
+        Auth::login($user);
+        // redirect to the specific page
+        return redirect("/jobs");
     }
 }
