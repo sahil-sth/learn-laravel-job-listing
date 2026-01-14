647b1c8ff25a66ed916e1ed2da8524f1d15d77ed
sahil-sth <saheel.sth99@gmail.com>
2026-01-13 22:52:52 -0700

    Add gate keeping for edit a job

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Job;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class JobController extends Controller
 { 
@@ -36,7 +38,8 @@ class JobController extends Controller
     }
     // shows the edit job page
     public function edit($id){
-        $job = Job::find($id);
+        $job = Job::findOrFail($id);
+        Gate::authorize("edit-job", $job);
         return view("jobs.edit", ["job" => $job]);
     }
     // actually edits the job
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use App\Models\Job;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // check for the authorization
+        Gate::define("edit-job", function(User $user, Job $job){
+            return $job->employer->user->is($user);
+        });
     }
 }
