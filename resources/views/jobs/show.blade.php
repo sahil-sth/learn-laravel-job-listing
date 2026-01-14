25bd5596cebdee2ecafa358ac0d44618c4a17905
sahil-sth <saheel.sth99@gmail.com>
2026-01-13 23:00:30 -0700

    Show edit button to owning users

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,7 +2,9 @@
   <x-slot name="heading">Job</x-slot>
   <h2 class="font-bold text-lg">Job title: {{ $job->title }}</h2>
   <p>This job pays: {{ $job->salary }}</p>
+  @can('edit-job', $job)
   <p class="mt-4">
     <x-button href="/jobs/{{ $job->id }}/edit">Edit a job</x-button>
   </p>
+  @endcan
 </x-layout>
